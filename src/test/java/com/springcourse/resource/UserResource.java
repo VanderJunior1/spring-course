895e7a8924ef448414408c9a15c23c1fbde0f7c9
VanderJunior1 <vanderjunior1@hotmail.com>
2020-12-19 20:28:22 -0300

    Criando classe UserResource e UserLoginDto

---
+++ src/test/java/com/springcourse/resource/UserResource.java
@@ -0,0 +1,62 @@
+package com.springcourse.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springcourse.domain.User;
+import com.springcourse.dto.UserLoginDto;
+import com.springcourse.services.UserService;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@Autowired
+	private UserService service;
+
+	@PostMapping
+	public ResponseEntity<User> save(@RequestBody User user) {
+		User createdUser = service.save(user);
+		return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+
+	}
+
+	@PostMapping("/login")
+	public ResponseEntity<User> login(@RequestBody UserLoginDto user) {
+		User loggedUser = service.login(user.getEmail(), user.getPassword());
+		return ResponseEntity.ok(loggedUser);
+		
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<User> update(@PathVariable(name = "id") Integer id, @RequestBody User user) {
+		user.setId(id);
+		User updatedUser = service.save(user);
+		return ResponseEntity.ok(updatedUser);
+
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<User> getById(@PathVariable(name = "id") Integer id) {
+		User user = service.getById(id);
+		return ResponseEntity.ok(user);
+
+	}
+
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		List<User> lista = service.findAll();
+		return ResponseEntity.ok(lista);
+	}
+
+}
